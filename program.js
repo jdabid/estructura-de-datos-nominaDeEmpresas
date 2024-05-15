2c1564f03e8807ce4290614f19695ee37e6b1317
David Castro <dabid.banegas@gmail.com>
2024-05-15 09:18:33 -0500

    commit 1 en branch subsidioRural - metodo subsidioRuralla

--- program.js
+++ program.js
@@ -1,13 +1,57 @@
 const readlineSync = require('readline-sync');
 
-/*const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi ${userName}!`);*/
-
 class Empleado {
-    nombreEmpleado = '';
-    salario = 0;
-    estrato = 0;
-    //rural = false;
+    #nombreEmpleado = '';
+    #salario = 0;
+    #estrato = 0;
+    #rural = 1;
+
+    set nombreEmpleado(nuevonombreEmpleado){
+        this.#nombreEmpleado = nuevonombreEmpleado;
+    }
+    get nombreEmpleado(){
+        return this.#nombreEmpleado;
+    }
+
+    set salario(nuevosalario){
+        this.#salario = nuevosalario;
+    }
+    get salario(){
+        return this.#salario;
+    }
+
+    set estrato(nuevoestrato){
+        this.#estrato = nuevoestrato;
+    }
+    get estrato(){
+        return this.#estrato;
+    }
+
+    set rural(nuevorural){
+        this.#rural = nuevorural;
+    }
+    get rural(){
+        return this.#rural;
+    }
+
+    constructor(nombreEmpleado, salario, estrato, rural){
+        if(nombreEmpleado == undefined){
+            throw new Error('El nombre del empleado es requerido');}
+
+        if(salario < 1 || isNaN(salario)){
+            throw new Error('Recuerda el salario del empleado no puede ser 0 o un valor negativo');}
+
+        if(estrato < 1 || estrato > 6 || isNaN(estrato)){
+            throw new Error('Recuerda que el estrato debe ser un valor numerico entre 1 y 6');
+        }
+        if(rural < 1 || rural > 2){
+            throw new Error('Recuerda que el campo rural debe ser si(1)/no(2)');
+        }
+        this.#nombreEmpleado = nombreEmpleado;
+        this.#salario = salario;
+        this.#estrato = estrato;
+        this.#rural = rural;
+    }
 }
 
 class NodoEmpleado {
@@ -45,11 +89,12 @@ class ListaEmpleados {
                 console.log(`Nombre: ${nodoTmp.valor.nombreEmpleado}:`);
                 console.log(`Salario: ${nodoTmp.valor.salario}:`);
                 console.log(`Estrato: ${nodoTmp.valor.estrato}:`);
+                console.log(`Rural: ${nodoTmp.valor.rural}:`);
                 nodoTmp = nodoTmp.siguiente;
                 i++;
             }
         }
-    }    
+    }
 
     nominaTotlaItems(){
         if(this.cabeza == null){
@@ -72,6 +117,7 @@ class ListaEmpleados {
         let salarioEstrato1 = 0;
         let salarioEstrato2 = 0;
         let salarioEstrato3 = 0;
+        let totalNominaSubsidioEstrato = 0;
 
         if(this.cabeza == null){
             console.log(`No hay empleados para mostrar, no hay nodos en la lista`);
@@ -80,55 +126,85 @@ class ListaEmpleados {
             let nodoTmp = this.cabeza;
             let i = 1;
             while(nodoTmp != null){
-                if(nodoTmp.valor.estrato === 1){
-                    nodoTmp.valor.salarioEstrato1 = nodoTmp.valor.salario * 0.15;
-                    console.log(`El salario del empleado ${nodoTmp.valor.nombreEmpleado} con estrato 1 es: ${nodoTmp.valor.salarioEstrato1}`);
+                if(nodoTmp.valor.estrato == 1){
+                    //nodoTmp.valor.salarioEstrato1 = nodoTmp.valor.salario * 0.15;
+                    nodoTmp.valor.salarioEstrato1 = nodoTmp.valor.salario + (nodoTmp.valor.salario * 0.15);
+                    console.log(`El salario del empleado ${nodoTmp.valor.nombreEmpleado} con subsidop por estrato 1 es: ${nodoTmp.valor.salarioEstrato1}`);
+                    salarioEstrato1 += nodoTmp.valor.salarioEstrato1;
                 }
-                else if(nodoTmp.valor.estrato === 2){
-                    nodoTmp.valor.salarioEstrato2 = nodoTmp.valor.salario * 0.10;
-                    console.log(`El salario del empleado ${nodoTmp.valor.nombreEmpleado} con estrato 2 es: ${nodoTmp.valor.salarioEstrato2}`);
+                else if(nodoTmp.valor.estrato == 2){
+                    nodoTmp.valor.salarioEstrato2 = nodoTmp.valor.salario + (nodoTmp.valor.salario * 0.10);
+                    console.log(`El salario del empleado ${nodoTmp.valor.nombreEmpleado} con subsidop por estrato 2 es: ${nodoTmp.valor.salarioEstrato2}`);
+                    salarioEstrato2 += nodoTmp.valor.salarioEstrato2;
                 }
-                else if(nodoTmp.valor.estrato === 3){
-                    nodoTmp.valor.salarioEstrato3 = nodoTmp.valor.salario * 0.05;
-                    console.log(`El salario del empleado ${nodoTmp.valor.nombreEmpleado} con estrato 3 es: ${nodoTmp.valor.salarioEstrato3}`);
+                else if(nodoTmp.valor.estrato == 3){
+                    nodoTmp.valor.salarioEstrato3 = nodoTmp.valor.salario + (nodoTmp.valor.salario * 0.05);
+                    console.log(`El salario del empleado ${nodoTmp.valor.nombreEmpleado} con subsidop por estrato 3 es: ${nodoTmp.valor.salarioEstrato3}`);
+                    salarioEstrato3 += nodoTmp.valor.salarioEstrato3;
                 }
                 else {
                     console.log(`El empleado ${nodoTmp.valor.nombreEmpleado} no tiene subsidio por concepto de estrato`);
+                    totalNominaSubsidioEstrato += nodoTmp.valor.salario;
                 }
+
                 nodoTmp = nodoTmp.siguiente;
                 i++;
-            }
+            }totalNominaSubsidioEstrato += salarioEstrato1 + salarioEstrato2 + salarioEstrato3;
+            console.log(`La nomina total de todos los empleados con subsidio por estrato es: ${totalNominaSubsidioEstrato}`);
         }
     }
 
-    
+    subsidioRural(){
+        let salarioSiRural = 0;
+        let totalNominaSubsidioRural = 0;
+
+        if(this.cabeza == null){
+            console.log(`No hay empleados para mostrar, no hay nodos en la lista`);
+        }
+        else {
+            let nodoTmp = this.cabeza;
+            let i = 1;
+            while(nodoTmp != null){
+                if(nodoTmp.valor.rural == 1){
+                    nodoTmp.valor.salarioSiRural = nodoTmp.valor.salario + 35000;
+                    console.log(`El salario del empleado ${nodoTmp.valor.nombreEmpleado} con subsidop rural es: ${nodoTmp.valor.salarioSiRural}`);
+                    salarioSiRural += nodoTmp.valor.salarioSiRural;
+                }
+                else {
+                    console.log(`El empleado ${nodoTmp.valor.nombreEmpleado} no vive en zona rural`);
+                    totalNominaSubsidioRural += nodoTmp.valor.salario;
+                }
+
+                nodoTmp = nodoTmp.siguiente;
+                i++;
+            }totalNominaSubsidioRural += salarioSiRural;
+            console.log(`La nomina total de todos los empleados con subsidio rural es: ${totalNominaSubsidioRural}`);
+        }
+    }
 }
 
 const listaEmpleados = new ListaEmpleados();
 
-const empleado1 = new Empleado();
-empleado1.nombreEmpleado = 'Pepe';
-empleado1.salario = 10000;
-empleado1.estrato = 1;
-listaEmpleados.insertar(empleado1);
+let continuar = true;
+while(continuar){
+    const nombreEmpleado = readlineSync.question('Ingrese el nombre del empleado: ');
+    const salario = +readlineSync.question('Ingrese el salario del empleado: ');
+    const estrato = +readlineSync.question('Ingrese el estrato del empleado: ');
+    const rural = readlineSync.question('El empleado vive en zona rural? (s(1)/n(2)): ');
 
-const empleado2 = new Empleado();
-empleado2.nombreEmpleado = 'Pedro';
-empleado2.salario = 20000;
-empleado2.estrato = 6;
-listaEmpleados.insertar(empleado2);
+    const empleado = new Empleado(nombreEmpleado, salario, estrato, rural);
+    listaEmpleados.insertar(empleado);
 
-const empleado3 = new Empleado();
-empleado3.nombreEmpleado = 'Juan';
-empleado3.salario = 3000;
-empleado3.estrato = 3;
-listaEmpleados.insertar(empleado3);
+    const respuesta = readlineSync.question('Desea agregar otro empleado? (s/n): ');
+    if(respuesta === 'n'){
+        continuar = false;
+    }
+}
 
 listaEmpleados.mostrarTodosLosEmpleados();
-//listaEmpleados.mostrarEmpleadoConMenorSalario();
-//listaEmpleados.nominaTotlaItems();
+listaEmpleados.nominaTotlaItems();
 listaEmpleados.subsidioEstrato();
-//listaEmpleados.subsidioEstrato();
+listaEmpleados.subsidioRural();
 
 
 
